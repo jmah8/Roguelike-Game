21bdef7ba492debc256e45d80f54141a4612b05e
jonMa99 <jonathanmah99@gmail.com>
2020-07-04 22:34:16 -0700

    Fixed untransparent background for player

--- src/game.py
+++ src/game.py
@@ -15,31 +15,37 @@ def loadImage(name, colorkey=None):
     except pygame.error as message:
         print('Cannot load image:', name)
         raise SystemExit(name)
-    image = image.convert()
+    image = image.convert_alpha()
     if colorkey is not None:
         if colorkey == -1:
             colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey, pygame.RLEACCEL)
     return image
+    
 
-# class Player(pygame.sprite.Sprite):
-#     def __init__(self):
-#         pygame.sprite.Sprite.__init__(self)
-#         self.image, self.rect = loadImage('Player.png', -1)
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = loadImage('Player.png', -1)
+        self.image
+        self.health = 3
+        self.velocity = (0, 0)
+        self.position = (800, 400)
 
+    def update(self):
+        self.rect.move(velocity)
 
 
 
 pygame.init()
-
 screen = pygame.display.set_mode(RESOLUTION)
+pygame.display.set_caption('My Pygame')
+
 
 background = loadImage('Background.jpg')
-background = pygame.transform.scale(background, RESOLUTION)
 screen.blit(background, TOPLEFT)
 
-
-player = loadImage('Player.jpg')
+player = loadImage('Player.png')
 screen.blit(player, PLAYERSTART)
 
 
@@ -47,13 +53,13 @@ position = player.get_rect().move(PLAYERSTART)
 
 pygame.display.flip()
 
-while True:
-    quit = False
+run = True
+while run:
     events = pygame.event.get()
     for event in events:
         screen.blit(background, position, position)
         if event.type == pygame.QUIT:
-            quit = True
+            run = False
             break
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
@@ -67,7 +73,5 @@ while True:
             screen.blit(player, position)
             pygame.display.flip()
             pygame.time.delay(10)
-    if quit:
-        break
 
 pygame.quit()
