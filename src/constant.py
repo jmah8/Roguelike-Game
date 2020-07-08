63f2c7fc5bd239ee7ff9b417a58269fffd0c8012
jonMa99 <jonathanmah99@gmail.com>
2020-07-07 19:10:48 -0700

    Fixed slime path

--- src/constant.py
+++ src/constant.py
@@ -5,4 +5,13 @@ TOPLEFT = (0, 0)
 
 # Image path
 CHARACTER = '16x16/heroes/knight/knight_idle_anim_f0.png'
-SLIME = '16x16/mobs/slime_idle_anim_f0.png'
+SLIME = 'mobs/slime_idle_anim_f0.png'
+
+FLOOR_1 = '16x16/tiles/floor/floor_1.png'
+FLOOR_2 = '16x16/tiles/floor/floor_2.png'
+FLOOR_3 = '16x16/tiles/floor/floor_3.png'
+FLOOR_4 = '16x16/tiles/floor/floor_4.png'
+FLOOR_5 = '16x16/tiles/floor/floor_side2.png'
+FLOOR_6 = '16x16/tiles/floor/floor_side3.png'
+FLOOR_7 = '16x16/tiles/floor/floor_side4.png'
+FLOOR_8 = '16x16/tiles/floor/floor_side5.png'
