f77fc9b7b5337a859c4d98513e3bff87a850dae0
jonMa99 <jonathanmah99@gmail.com>
2020-08-13 00:08:08 -0700

    Fixed minimap not removing dead enemies and enemies are now ontop of items

--- src/minimap.py
+++ src/minimap.py
@@ -97,8 +97,8 @@ def draw_minimap_generated_map(game):
     _draw_minimap_walls_generated_map(game, scale_factor_x, scale_factor_y)
     _draw_minimap_rooms_generated_map(game, scale_factor_x, scale_factor_y)
     _draw_unseen_tile_generated_map(game, scale_factor_x, scale_factor_y)
-    _draw_minimap_enemies_in_fov_both_map(game, scale_factor_x, scale_factor_y)
     _draw_minimap_items_both_map(game, scale_factor_x, scale_factor_y)
+    _draw_minimap_enemies_in_fov_both_map(game, scale_factor_x, scale_factor_y)
     _draw_minimap_player_generated_map(game, scale_factor_x, scale_factor_y)
 
 
@@ -119,8 +119,8 @@ def draw_minimap_loaded_map(game):
     scale_factor_y = SPRITE_SIZE / scale_factor_height
 
     _draw_minimap_floor_and_walls_loaded_map(game, scale_factor_x, scale_factor_y)
-    _draw_minimap_enemies_in_fov_both_map(game, scale_factor_x, scale_factor_y)
     _draw_minimap_items_both_map(game, scale_factor_x, scale_factor_y)
+    _draw_minimap_enemies_in_fov_both_map(game, scale_factor_x, scale_factor_y)
     _draw_minimap_player_loaded_map(game, scale_factor_x, scale_factor_y)
 
 
@@ -211,7 +211,7 @@ def _draw_minimap_enemies_in_fov_both_map(game, scale_factor_x, scale_factor_y):
         scale_factor_x (int): How much to scale x by
         scale_factor_y (int): How mucg to scale y by
     """
-    for enemy in game.ENEMIES:
+    for enemy in game.enemy_group:
         if game.fov[enemy.y][enemy.x]:
             pygame.draw.rect(game.surface, RED,
                              ((enemy.rect.topleft[0] / scale_factor_x),
